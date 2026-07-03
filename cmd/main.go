cd884a6b25e1e832149171a553e1e7de827dd894
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-07-03 22:15:15 +0530

    target 8081(hardcoding for now)

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/nomen06/load-balancer/internal/proxy"
+)
+
+func main() {
+	target := "http://localhost:8081" // will fix this hardcoding later
+	p, err := proxy.NewProxy(target)
+	if err != nil {
+		log.Fatalf("initialisation failed for proxy : %v", err)
+	}
+	http.Handle("/", p)
+	log.Println("Reverse proxy initialisation on our port:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
