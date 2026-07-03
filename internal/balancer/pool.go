89fc49421b02ef77b49709e3a8493619897ad4ad
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-07-04 00:29:53 +0530

    skipping the dead backends

--- internal/balancer/pool.go
+++ internal/balancer/pool.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
 	"sync/atomic"
 
 	"github.com/nomen06/load-balancer/internal/proxy"
@@ -12,7 +13,21 @@ import (
 // using atomic here because it is much much faster for simple increment operatons rather than using mutexes
 type Backend struct {
 	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
 	ReverseProxy *httputil.ReverseProxy
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+func (b *Backend) Isalive() bool {
+	b.mux.RLock()
+	alive := b.Alive
+	b.mux.RUnlock()
+	return alive
 }
 
 type Serverpool struct {
@@ -41,13 +56,26 @@ func (s *Serverpool) NextInd() int {
 }
 
 func (s *Serverpool) Nextavl() *Backend {
-	if len(s.backends) == 0 {
-		return nil
-	}
+	// if len(s.backends) == 0 {
+	// 	return nil
+	// }
 	next := s.NextInd()
-	return s.backends[next]
+	l := len(s.backends)
+	// return s.backends[next]
+
+	// checking for the alive ones
+	for i := 0; i < l; i++ {
+		ind := (next + i) % l
+		if s.backends[ind].Isalive() {
+			if i != 0 {
+				atomic.StoreUint64(&s.current, uint64(ind))
+			}
+			return s.backends[ind]
+		}
+	}
+	return nil
 }
-func (s *Serverpool) Httpserve(w http.ResponseWriter, r *http.Request) {
+func (s *Serverpool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	peer := s.Nextavl()
 	if peer != nil {
 		peer.ReverseProxy.ServeHTTP(w, r)
