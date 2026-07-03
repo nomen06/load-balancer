7a943b4e5c82a7875838e76729dc63941ba32c64
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-07-03 23:42:02 +0530

    error fixed with the round robin

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,37 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/nomen06/load-balancer/internal/proxy"
+	"github.com/nomen06/load-balancer/internal/balancer"
 )
 
 func main() {
-	target := "http://localhost:8081" // will fix this hardcoding later
-	p, err := proxy.NewProxy(target)
-	if err != nil {
-		log.Fatalf("initialisation failed for proxy : %v", err)
+	// target := "http://localhost:8081" // will fix this hardcoding later
+	// p, err := proxy.NewProxy(target)
+	// if err != nil {
+	// 	log.Fatalf("initialisation failed for proxy : %v", err)
+	// }
+	// http.Handle("/", p)
+	// log.Println("Reverse proxy initialisation on our port:8080")
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatalf("server failed: %v", err)
+	// }
+
+	//connecting for multiple backends
+	serverpool := &balancer.Serverpool{}
+	servers := []string{
+		"http://localhost:8081",
+		"http://localhost:8082",
+		"http://localhost:8083",
 	}
-	http.Handle("/", p)
-	log.Println("Reverse proxy initialisation on our port:8080")
+	for _, srv := range servers {
+		if err := serverpool.AddBackend(srv); err != nil {
+			log.Fatalf("Could not parse backend URL : %v", err)
+		}
+		log.Printf("Configured backend: %s,srv")
+	}
+	http.Handle("/", serverpool)
+	log.Println("Load balancer running on : 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("server failed: %v", err)
+		log.Fatalf("Server failed: %v", err)
 	}
 }
